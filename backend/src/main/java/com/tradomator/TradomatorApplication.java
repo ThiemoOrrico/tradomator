5cbb14c5dc64e1eff43c817ce5582b884fe82f21
Thiemo Orrico <thiemobile@o2online.de>
2021-10-10 21:43:30 +0200

    Initial commit

---
+++ backend/src/main/java/com/tradomator/TradomatorApplication.java
@@ -0,0 +1,13 @@
+package com.tradomator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradomatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TradomatorApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/tradomator/TradomatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tradomator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradomatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
