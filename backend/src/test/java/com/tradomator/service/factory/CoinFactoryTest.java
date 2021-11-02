994af7ee5670ba6ad6d7ec432a271d66e39f2fe5
Thiemo Orrico <thiemobile@o2online.de>
2021-11-02 21:09:45 +0100

    Add pom dependencies mockServer for ServiceApi Test mocking WebClient

---
+++ backend/src/test/java/com/tradomator/service/factory/CoinFactoryTest.java
@@ -0,0 +1,12 @@
+package com.tradomator.service.factory;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CoinFactoryTest {
+
+    @Test
+    void mapToCoin() {
+    }
+}
