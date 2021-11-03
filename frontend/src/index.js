1242324d5c6ba9975fc12fb1afccd94b7b9a80d1
Thiemo Orrico <thiemobile@o2online.de>
2021-11-03 22:34:58 +0100

    Add App Bar, insert global Style colorscheme blue - app is functional

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ThemeProvider } from '@mui/material/styles';
+import {GlobalStyle} from "./GlobalStyle";
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <GlobalStyle></GlobalStyle>
+      <App />
+
   </React.StrictMode>,
   document.getElementById('root')
 );
