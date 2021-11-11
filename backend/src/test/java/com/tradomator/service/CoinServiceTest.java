4dfd88aeb5f6a1ef29d43490b78377adbf878461
Thiemo Orrico <thiemobile@o2online.de>
2021-11-11 11:00:31 +0100

    Add service Unit test getAllCOinData. All tests pass

--- backend/src/test/java/com/tradomator/service/CoinServiceTest.java
+++ backend/src/test/java/com/tradomator/service/CoinServiceTest.java
@@ -10,20 +10,22 @@ import com.tradomator.service.coingeckoapi.CoingeckoApiService;
 import com.tradomator.service.mapperapi.CoingeckoMapper;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
 
 import org.hamcrest.Matchers;
+import org.springframework.http.ResponseEntity;
+
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.mockito.Mockito.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
-
-
 
 
 class CoinServiceTest {
@@ -50,7 +52,7 @@ class CoinServiceTest {
         cgeckoApiCoin1.setImage(new ImageApi("https:/fakeURL"));
         cgeckoApiCoin1.setLastUpdated(currentDateTime);
 
-        when(coingeckoApiService.getCgeckoCoinById("someBitcoinId")).thenReturn( cgeckoApiCoin1);
+        when(coingeckoApiService.getCgeckoCoinById("someBitcoinId")).thenReturn(cgeckoApiCoin1);
 
         CurrentPrice currentPrice1 = new CurrentPrice((float) 60000.12345);
 
@@ -69,6 +71,65 @@ class CoinServiceTest {
 
         //THEN
         assertThat(actual, is(coinIdCard1));
+
+    }
+
+    @Test
+    void getAllCoinData_shouldreturnListOfCoins() {
+        //GIVEN
+        MarketDataApi marketDataApi1 = new MarketDataApi(new CurrentPriceApi(60000.12345f, 45000.54321f));
+
+        LocalDateTime currentDateTime = LocalDateTime.now();
+
+        CgeckoApiCoin cgeckoApiCoin1 = new CgeckoApiCoin();
+        cgeckoApiCoin1.setId("bitcoin");
+        cgeckoApiCoin1.setSymbol("btc");
+        cgeckoApiCoin1.setName("Bitcoin");
+        cgeckoApiCoin1.setMarketData(marketDataApi1);
+        cgeckoApiCoin1.setImage(new ImageApi("https:/fakeURL"));
+        cgeckoApiCoin1.setLastUpdated(currentDateTime);
+
+        CgeckoApiCoin cgeckoApiCoin2 = new CgeckoApiCoin();
+        cgeckoApiCoin2.setId("ethereum");
+        cgeckoApiCoin2.setSymbol("etc");
+        cgeckoApiCoin2.setName("Ethereum");
+        cgeckoApiCoin2.setMarketData(marketDataApi1);
+        cgeckoApiCoin2.setImage(new ImageApi("https:/fakeURLEthereum"));
+        cgeckoApiCoin2.setLastUpdated(currentDateTime);
+
+
+        when(coingeckoApiService.getAllCgeckoCoinInfo()).thenReturn( new CgeckoApiCoin[]{cgeckoApiCoin1, cgeckoApiCoin2});
+
+
+        CurrentPrice currentPrice1 = new CurrentPrice((float) 60000.12345);
+
+        CoinIdCard coinIdCard1 = new CoinIdCard();
+        coinIdCard1.setId("bitcoin");
+        coinIdCard1.setSymbol("btc");
+        coinIdCard1.setName("Bitcoin");
+        coinIdCard1.setCurrentPrice(currentPrice1);
+        coinIdCard1.setImageUrl("https:/fakeURL");
+        coinIdCard1.setLastUpdated(currentDateTime);
+
+        CurrentPrice currentPrice2 = new CurrentPrice((float) 4325.543);
+
+        CoinIdCard coinIdCard2 = new CoinIdCard();
+        coinIdCard1.setId("ethereum");
+        coinIdCard1.setSymbol("eth");
+        coinIdCard1.setName("Ethereum");
+        coinIdCard1.setCurrentPrice(currentPrice2);
+        coinIdCard1.setImageUrl("https:/fakeURL");
+        coinIdCard1.setLastUpdated(currentDateTime);
+
+
+        when(coingeckoMapper.mapToCoins(new CgeckoApiCoin[] {cgeckoApiCoin1, cgeckoApiCoin2})).thenReturn(
+                List.of(coinIdCard1, coinIdCard2));
+
+        //WHEN
+        List<CoinIdCard> actual = coinService.getAllCoinData();
+
+        //THEN
+        assertThat(actual, is(List.of(coinIdCard1, coinIdCard2)));
 
     }
 
