ccf91ce2b4024901c5e35c003bf209fa3a166ded
Thiemo Orrico <thiemobile@o2online.de>
2021-11-20 23:10:59 +0100

    Add BadCredentials Exception at Controller Advisor

--- backend/src/main/java/com/tradomator/controller/exception/ControllerAdvisor.java
+++ backend/src/main/java/com/tradomator/controller/exception/ControllerAdvisor.java
@@ -3,6 +3,7 @@ package com.tradomator.controller.exception;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -31,6 +32,16 @@ public class ControllerAdvisor extends ResponseEntityExceptionHandler {
         ApiError apiError = new ApiError("Given input is not processable!", ex.getMessage());
 
         return new ResponseEntity<>(apiError, HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<ApiError> handleIllegalArgumentException(BadCredentialsException ex){
+
+        log.error("Username and Password are not valid!", ex);
+
+        ApiError apiError = new ApiError("Username and Password are not valid!", ex.getMessage());
+
+        return new ResponseEntity<>(apiError, HttpStatus.UNAUTHORIZED);
     }
 
     @ExceptionHandler(Throwable.class)
